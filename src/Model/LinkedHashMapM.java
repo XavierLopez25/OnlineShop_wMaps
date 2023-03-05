ed31a5f7fd64a1127ded18ca63b29bf81d1b69c6
XavierLopez25 <lop22716@uvg.edu.gt>
2023-03-05 12:39:22 -0600

    Clase LinkedHashMapM con funciones de HashMap e implementación de la interfaz Map finalizada.

--- src/Model/LinkedHashMapM.java
+++ src/Model/LinkedHashMapM.java
@@ -1,4 +1,76 @@
 package Model;
 
-public class LinkedHashMapM {
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class LinkedHashMapM implements Map {
+
+    public LinkedHashMap<Object, Object> myLinkedHashMap;
+
+    public LinkedHashMapM() {
+        myLinkedHashMap = new LinkedHashMap<>();
+    }
+
+    @Override
+    public int size() {
+        return myLinkedHashMap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return myLinkedHashMap.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return myLinkedHashMap.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return myLinkedHashMap.containsKey(value);
+    }
+
+    @Override
+    public Object get(Object key) {
+        return myLinkedHashMap.get(key);
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return myLinkedHashMap.put(key, value);
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return myLinkedHashMap.remove(key);
+    }
+
+    @Override
+    public void putAll(Map m) {
+        myLinkedHashMap.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        myLinkedHashMap.clear();
+    }
+
+    @Override
+    public Set keySet() {
+        return myLinkedHashMap.keySet();
+    }
+
+    @Override
+    public Collection values() {
+        return myLinkedHashMap.values();
+    }
+
+    @Override
+    public Set<Map.Entry<Object, Object>> entrySet() {
+        return myLinkedHashMap.entrySet();
+    }
+
 }
