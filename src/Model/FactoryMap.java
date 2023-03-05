7006f1c60824c9176c17a7eb3bcf840a6f9061d5
XavierLopez25 <lop22716@uvg.edu.gt>
2023-03-05 12:47:27 -0600

    Clase FactoryMap creada para HashMapM, TreeMapM y LinkedHashMapM.

--- src/Model/FactoryMap.java
+++ src/Model/FactoryMap.java
@@ -1,4 +1,18 @@
 package Model;
 
+import java.util.Map;
+
 public class FactoryMap {
+
+    public Map getMapImplementation(String selection){
+
+        return switch (selection.toLowerCase()) {
+            case "hashmap" -> new HashMapM();
+            case "treemap" -> new TreeMapM();
+            case "linkedhashmap" -> new LinkedHashMapM();
+            default -> null;
+        };
+
+    }
+
 }
