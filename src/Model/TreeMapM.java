6e7b021ca3dbd5ce75887b5a08e2626026edbbd4
XavierLopez25 <lop22716@uvg.edu.gt>
2023-03-05 12:30:25 -0600

    Clase TreeMapM con funciones de HashMap e implementación de la interfaz Map finalizada.

--- src/Model/TreeMapM.java
+++ src/Model/TreeMapM.java
@@ -1,4 +1,75 @@
 package Model;
 
-public class TreeMapM {
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class TreeMapM implements Map {
+
+    public TreeMap<Object, Object> myTreeMap;
+
+    public TreeMapM(){
+        myTreeMap = new TreeMap<>();
+    }
+
+    @Override
+    public int size() {
+        return myTreeMap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return myTreeMap.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return myTreeMap.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return myTreeMap.containsKey(value);
+    }
+
+    @Override
+    public Object get(Object key) {
+        return myTreeMap.get(key);
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return myTreeMap.put(key, value);
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return myTreeMap.remove(key);
+    }
+
+    @Override
+    public void putAll(Map m) {
+        myTreeMap.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        myTreeMap.clear();
+    }
+
+    @Override
+    public Set keySet() {
+        return myTreeMap.keySet();
+    }
+
+    @Override
+    public Collection values() {
+        return myTreeMap.values();
+    }
+
+    @Override
+    public Set<Entry<Object, Object>> entrySet() {
+        return myTreeMap.entrySet();
+    }
 }
